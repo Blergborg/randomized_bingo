36b2da43962b1ae177a7c7305578f349ce251040
Blergborg <pprater92@gmail.com>
2024-02-25 23:28:50 -0800

    Setup example to be clearer.

--- src/BingoApp.tsx
+++ src/BingoApp.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import ShuffleWords from "./components/BingoCard";
 import './App.css'
 
-const WORDS: string[] = []
-for(let i=1; i <= 25; i++) {
-  WORDS.push(`${i}`)
+
+
+// *** DELETE THIS if setting your own WORDS array ***
+// Fill out WORDS array w/ numbers for example
+const exampleList = (range: number): string[] => {
+  let temp = []
+  for(let i=1; i <= range; i++) {
+    temp.push(`${i}`)
+  }
+  return temp
 }
+
+// *** Hard-code list of strings you want to use here. ***
+const WORDS: string[] = exampleList(99)
 
 function BingoApp() {
   return (
